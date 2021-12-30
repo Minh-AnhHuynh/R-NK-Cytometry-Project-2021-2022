26b66c51a13e55f670d970941a4befb7af89f780
Minh-AnhHuynh <minh-anh.huynh@etu.u-paris.fr>
2021-12-30 14:12:31 +0100

    Organization changes, folder changed from 03_spade_analysis to 04_SPADEVizR

--- 01_script/Code-SPADEVizR.R
+++ 01_script/Code-SPADEVizR.R
@@ -1,56 +1,61 @@
-librarian::shelf(SPADEVizR, data.table, ggplot2, stringr, dplyr, flowCore)
-
-### DATA LOADING
-
+# 1. Setup -------------------------------------------------------------------
+# Make sure to install librarian before launching the script.
+# Make sure to have opened with an R project file first.
+librarian::shelf(tchitchek-lab/SPADEVizR, data.table, ggplot2, stringr, dplyr, flowCore)
+set.seed(123) # for reproducibility
+seed_number = 123
+# 2. Data loading ------------------------------------------------------------
 clustering_markers = read.delim('./02_data/NK_panel.txt')
+# Filtering markers
 excluded_markers = clustering_markers[c(1:7,18,24,29,39,43,44,45),1]
 excluded_markers2 = c("File Number", "density", "cells-(Ir191)Di", "cells-(Ir193)Di")
 excluded_markers = c(excluded_markers, excluded_markers2)
-# loads the SPADE results contained in the folder "SPADE" on the Desktop
 
-################
-k_value = 100 ##
-################
+## Select k_value
+k_value = 100 
 
+## Load SPADE results
 output_dir = paste0("./03_spade_analysis/spade_k", k_value)
-
 spade_results = importResultsFromSPADE(output_dir,
                                        exclude.markers = excluded_markers)
-# Check if spade_results works :
+# To check if spade_results is correct :
 # head(spade_results@cluster.phenotypes)
 
-# génération de la heatmap 
+
+# 3. Quality Control ---------------------------------------------------------
+
+# Generate a QC report to detect cell cluster phenotype with uniform marker distribution expressions 
+accuracyQC <- qcUniformClusters(spade_results,
+                                density.PDFfile = paste0("./04_SPADEVizR/QualityControls/UniformClusters_density_k",k_value,"_seed", seed_number,".pdf"),
+                                heatmap.PDFfile = paste0("./04_quality_controls/UniformClusters_heatmap_k",k_value,"_seed",seed_number, ".pdf"),
+                                uniform.test   = "unimodality")
+
+# Display the percentage of accuracy clusters
+print(accuracyQC$perc)
+
+# Save the accuracy matrix in a text file
+write.table(accuracyQC$accuracy.matrix, paste0("./04_quality_controls/UniformClusters-accuracy-matrix_k_", k_value,".txt"),quote=FALSE,sep="\t",col.names=NA)
+
+# Generate a QC report to detect cell cluster phenotypes with low number of cell clusters 								  
+smallclusterQC <- qcSmallClusters(spade_results, PDFfile = paste0("./04_quality_controls/QCreport-SmallClusters_heatmap_k_",k_value,".pdf"), th = 500)
+
+# Display the percentage of cell cluster phenotypes with low number of cell clusters 
+print(smallclusterQC$perc)
+
+# Save the small cluster matrix in a text file
+write.table(smallclusterQC$small.clusters, paste0("./04_quality_controls/UniformClusters-smallclusters-matrix_k_", k_value,".txt"),quote=FALSE,sep="\t",col.names=NA)
+###
+
+
+# 4. Analysis and Figures ----------------------------------------------------
+
+## 4.1 Heatmap generation ====================================================
 
 pdf(paste0("./05_figures/heatmap_spade_k_",k_value,".pdf"), height=10,width=17)
 heatmapViewer(spade_results)
 dev.off()
 
 
-### QC CONTROL
-set.seed(123) #for reproducibility
-
-# generates a QC report to detect cell cluster phenotypes with uniform marker distribution expressions 
-accuracyQC <- qcUniformClusters(spade_results,
-                                density.PDFfile = paste0("./04_quality_controls/UniformClusters_density_k_",k_value,"seed",".pdf"),
-                                heatmap.PDFfile = paste0("./04_quality_controls/UniformClusters_heatmap_k_",k_value,"seed",".pdf"),
-                                uniform.test   = "unimodality")
-
-
-# displays the percentage of accuracy clusters
-print(accuracyQC$perc)
-
-# saves the accuracy matrix in a text file
-write.table(accuracyQC$accuracy.matrix, paste0("./04_quality_controls/UniformClusters-accuracy-matrix_k_", k_value,".txt"),quote=FALSE,sep="\t",col.names=NA)
-
-# generates a QC report to detect cell cluster phenotypes with low number of cell clusters 								  
-smallclusterQC <- qcSmallClusters(spade_results, PDFfile = paste0("./04_quality_controls/QCreport-SmallClusters_heatmap_k_",k_value,".pdf"), th = 500)
-
-# displays the percentage of cell cluster phenotypes with low number of cell clusters 
-print(smallclusterQC$perc)
-
-# saves the small cluster matrix in a text file
-write.table(smallclusterQC$small.clusters, paste0("./04_quality_controls/UniformClusters-smallclusters-matrix_k_", k_value,".txt"),quote=FALSE,sep="\t",col.names=NA)
-###
 
 
 
@@ -59,21 +64,24 @@ write.table(smallclusterQC$small.clusters, paste0("./04_quality_controls/Uniform
 
 
 
-### DAC IDENTIFICATION 
+## 4.2 Identification of Differentially Abundant Clusters ==========================================================================
 
-# creation d'une dataframe avec ligne = cellule et colonne = marqueur
-# fs <- read.flowSet(path = "./02_data/NK_FCS/")
+### 4.2.1 Data frame creation : row = cell ; col = markers ##########################################################################
+# This step is done in order to get the assignment data.frame without selecting samples manually
+
+#Load data using flowSet
 files = list.files("./02_data/NK_FCS/", full.names = TRUE)
 FlowSet = read.flowSet(files)
 
-# sélection des marqueurs d'intérêt
+# Select markers of interest
 clustering_markers = read.delim('./02_data/NK_panel.txt')
 markers2norm = clustering_markers[-c(1:7,18,24,29,39,43:45),1]
 
-# transformation arcsinh des marqueurs
+# Arcsinh transformation of markers to be able to use Flowset
 ArcTrans = arcsinhTransform()
 TransList = transformList(from = markers2norm, tfun = ArcTrans)
 FlowSet = TransList %on% FlowSet
+# Generation of files
 data = data.frame()
 for (i in 1:length(FlowSet)) {
   print(paste0("fichier numero ", i))
@@ -84,28 +92,35 @@ for (i in 1:length(FlowSet)) {
   
 }
 
-####Création des différentes conditions pour chaque échantillon
+### 4.2.2 Assigning sample biological conditions (metadata information) ####################
 assignments = data.frame(bc = str_sub(data$sample,1,8),
                          tp = str_sub(data$sample,1,8),
                          #hour = str_sub(data$sample,6,8),
                          ind = str_sub(data$sample,10,14),
                          sample_names = str_sub(data$sample,1,23))
 
-#Tri des samples pour obtenir 42 echantillons
-assignments = assignments %>% group_by(sample_names) %>% slice(1) 
-# Mettre sample_names dans row names
+# Filtering samples to obtain only 42 values corresponding to 42 samples
+assignments = assignments %>% group_by(sample_names) %>% slice(1)
+
+# Put sample names in row names
 sample = assignments$sample_names
 assignments[4]=NULL
 row.names(assignments) = sample
 
 # Put assignment data.frame tidied into spade_results
-spade_results <- assignContext(spade_results, assignments = assignments)
+spade_results = assignContext(spade_results, assignments = assignments)
 
 
-# selects macaque samples before prime vaccination manually
+### 4.2.3 Select monkey samples according to their conditions respectively : ##########################################################################
+
+# Before Prime
 condition_BP = c("BPD19H00_BB078_CD3-CD8+", "BPD19H00_BB231_CD3-CD8+", "BPD19H00_BC641_CD3-CD8+", "BPD19H00_BD620_CD3-CD8+")
-condition_PB = c("PBD00H00_BB078_CD3-CD8+", "PBD00H00_BB231_CD3-CD8+", "PBD00H00_BD620_CD3-CD8+", "PBD00H03_BB078_CD3-CD8+", "PBD00H03_BB231_CD3-CD8+", "PBD00H03_BC641_CD3-CD8+", "PBD00H06_BB078_CD3-CD8+", "PBD00H06_BB231_CD3-CD8+", "PBD00H06_BC641_CD3-CD8+", "PBD00H06_BD620_CD3-CD8+", "PBD01H00_BB078_CD3-CD8+", "PBD01H00_BB231_CD3-CD8+", "PBD01H00_BC641_CD3-CD8+", "PBD01H00_BD620_CD3-CD8+", "PBD03H00_BB078_CD3-CD8+", "PBD03H00_BB231_CD3-CD8+", "PBD03H00_BD620_CD3-CD8+")
+
+# Post Prime
 condition_PP = c("PPD00H00_BB078_CD3-CD8+", "PPD00H00_BB231_CD3-CD8+", "PPD00H00_BD620_CD3-CD8+", "PPD00H03_BB078_CD3-CD8+", "PPD00H03_BB231_CD3-CD8+", "PPD00H03_BC641_CD3-CD8+", "PPD00H03_BD620_CD3-CD8+", "PPD00H06_BB078_CD3-CD8+", "PPD00H06_BB231_CD3-CD8+", "PPD00H06_BC641_CD3-CD8+", "PPD01H00_BB078_CD3-CD8+", "PPD01H00_BB231_CD3-CD8+", "PPD01H00_BC641_CD3-CD8+", "PPD01H00_BD620_CD3-CD8+", "PPD03H00_BB078_CD3-CD8+", "PPD03H00_BB231_CD3-CD8+", "PPD03H00_BC641_CD3-CD8+", "PPD14H00_BB078_CD3-CD8+", "PPD14H00_BB231_CD3-CD8+", "PPD14H00_BC641_CD3-CD8+", "PPD14H00_BD620_CD3-CD8+")
+# Post Boost
+condition_PB = c("PBD00H00_BB078_CD3-CD8+", "PBD00H00_BB231_CD3-CD8+", "PBD00H00_BD620_CD3-CD8+", "PBD00H03_BB078_CD3-CD8+", "PBD00H03_BB231_CD3-CD8+", "PBD00H03_BC641_CD3-CD8+", "PBD00H06_BB078_CD3-CD8+", "PBD00H06_BB231_CD3-CD8+", "PBD00H06_BC641_CD3-CD8+", "PBD00H06_BD620_CD3-CD8+", "PBD01H00_BB078_CD3-CD8+", "PBD01H00_BB231_CD3-CD8+", "PBD01H00_BC641_CD3-CD8+", "PBD01H00_BD620_CD3-CD8+", "PBD03H00_BB078_CD3-CD8+", "PBD03H00_BB231_CD3-CD8+", "PBD03H00_BD620_CD3-CD8+")
+
 
 #condition_BP = spade_results@assignments[grepl("^BP", rownames(spade_results@assignments)),1]
 
@@ -122,42 +137,57 @@ condition_PP = c("PPD00H00_BB078_CD3-CD8+", "PPD00H00_BB231_CD3-CD8+", "PPD00H00
 #condition_PB = assignments$sample_names[startsWith(assignments$sample_names, "PB")]
 
 
-# Differentially expressed clusters
-# Volcano plot : BP vs PP
-resultsDAC <- identifyDAC(spade_results, 
-                          condition1 = condition_BP, condition2 = condition_PP, 
-                          th.pvalue = 0.05, 
-                          th.fc = 2, 
-                          method.paired = FALSE)
 
-export(resultsDAC, filename = paste0("./03_spade_analysis/SPADEVizR/resultsDAC_k", k_value,"_BPvsPP.txt"))
+## 4.3 Volcano plot  =====================================================
+# Volcano plot Condition BP vs PP 
+resultsDAC <- identifyDAC(
+  spade_results,
+  condition1 = condition_BP,
+  condition2 = condition_PP,
+  th.pvalue = 0.05,
+  th.fc = 2,
+  method.paired = FALSE
+)
+export(resultsDAC, filename = paste0("./04_SPADEVizR/QualityControls/resultsDAC_k", k_value,"_BPvsPP.txt"))
 pdf(paste0("./05_figures/VolcanoDAC_k_",k_value,"_BPvsPP.pdf"), height=10,width=17)
 volcanoViewer(resultsDAC)
 dev.off()
 
-#Volcano plot PP v PB
-resultsDAC <- identifyDAC(spade_results, 
-                          condition1 = condition_PP, condition2 = condition_PB, 
-                          th.pvalue = 0.05, 
-                          th.fc = 2, 
-                          method.paired = FALSE)
-export(resultsDAC, filename = paste0("./03_spade_analysis/SPADEVizR/resultsDAC_k", k_value,"_PPvsPB.txt"))
+# Volcano plot Condition PP v PB
+resultsDAC <- identifyDAC(
+  spade_results,
+  condition1 = condition_PP,
+  condition2 = condition_PB,
+  th.pvalue = 0.05,
+  th.fc = 2,
+  method.paired = FALSE
+)
+export(resultsDAC, filename = paste0("./04_SPADEVizR/QualityControls/resultsDAC_k", k_value,"_PPvsPB.txt"))
 pdf(paste0("./05_figures/VolcanoDAC_k_",k_value,"_PPvsPB.pdf"), height=10,width=17)
 volcanoViewer(resultsDAC)
 dev.off()
 
-
-
-# Volcano plot BP vs PB
-resultsDAC <- identifyDAC(spade_results, 
-                          condition1 = condition_BP, condition2 = condition_PB, 
-                          th.pvalue = 0.05, 
-                          th.fc = 2, 
-                          method.paired = FALSE)
-export(resultsDAC, filename = paste0("./03_spade_analysis/SPADEVizR/resultsDAC_k", k_value,"BPvsPB"))
+# Volcano plot Condition BP vs PB
+resultsDAC <- identifyDAC(
+  spade_results,
+  condition1 = condition_BP,
+  condition2 = condition_PB,
+  th.pvalue = 0.05,
+  th.fc = 2,
+  method.paired = FALSE
+)
+export(resultsDAC, filename = paste0("./04_SPADEVizR/QualityControls/resultsDAC_k", k_value,"BPvsPB"))
 pdf(paste0("./05_figures/VolcanoDAC_k_",k_value,"_BPvsPB.pdf"), height=10,width=17)
 volcanoViewer(resultsDAC)
 dev.off()
+
+
+
+
+
+
+
+
 
 
 
